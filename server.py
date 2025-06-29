1b3890fd1f00bf8ab40a61b5e5065104e70b3374
constantijn <cdemeer@fim.uni-passau.de>
2025-06-29 21:14:30 +0200

    season swap

--- server.py
+++ server.py
@@ -28,34 +28,66 @@ def index():
     if request.method == 'GET':
         return get_file("maps.html")
     else:
-        data = request.json
-        if "map" not in data:
+        args = request.json
+        if "map" not in args:
             return post_error("No action provided!")
-        map = data["map"]
-        maps = get_data(MAPS_PATH)
+        map = args["map"]
+        season_name = args["season"]
+        data = get_data(MAPS_PATH)
+        if season_name == None:
+            season = data[next(iter(data))]
+        elif season_name in data:
+            season = data[season_name]
+        else:
+            return post_error("Invalid season name")
+        maps = season["maps"]
         if map not in maps:
             return post_error("Unknown map!")
-        t_played = maps[map]["played"] + data["change"]
+        t_played = data[season_name]["maps"][map]["played"] + args["change"]
         if t_played < 0:
             t_played = 0
-        maps[map]["played"] = t_played
-        write_data(MAPS_PATH, maps)
+        data[season_name]["maps"][map]["played"] = t_played
+        write_data(MAPS_PATH, data)
         return POST_SUCCESS
         
 @app.route('/reset', methods=['POST'])
 def reset():
     if request.method == 'POST':
-        maps = get_data(MAPS_PATH)
+        args = request.json
+        if "season" not in args:
+            return post_error("No season provided!")
+        season_name = args["season"]
+        data = get_data(MAPS_PATH)
+        if season_name == None:
+            season = data[next(iter(data))]
+        elif season_name in data:
+            season = data[season_name]
+        else:
+            return post_error("Invalid season name")
+        maps = data[season_name]["maps"]
         for map in maps:
-            maps[map]["played"] = 0
-        write_data(MAPS_PATH, maps)
+            data[season_name]["maps"][map]["played"] = 0
+        write_data(MAPS_PATH, data)
         return POST_SUCCESS
     return post_error("Invalid method")
     
-@app.route('/maps', methods=['GET'])
+@app.route('/maps', methods=['POST'])
 def maps():
-    if request.method != 'GET': return post_error("Invalid method")
-    maps = get_data(MAPS_PATH)
-    return {"success": True, "maps": maps, "total": sum([maps[map]["played"] for map in maps])}
+    if request.method != 'POST': return post_error("Invalid method")
+    args = request.json
+    if "season" not in args:
+        return post_error("No season provided!")
+    season_name = args["season"]
+    data = get_data(MAPS_PATH)
+    if season_name == None:
+        season = data[next(iter(data))]
+    elif season_name in data:
+        season = data[season_name]
+    else:
+        return post_error("Invalid season name")
+    maps = season["maps"]
+    ret = {"success": True, "maps": maps, "total": sum([maps[map]["played"] for map in maps]), "seasons": [{"value": key, "name": data[key]["name"]} for key in data.keys()], "period": {"from": season["date-from"], "until": season["date-to"]}}
+    print(ret)
+    return ret
     
 app.run()
